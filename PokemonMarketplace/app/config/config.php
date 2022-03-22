d709f78c4b58f12af0195e7f5e7e597e9511dde1
Reimar Rosas <reimarrosas0208@gmail.com>
2022-03-22 13:19:01 -0400

    Setup the Initial Files
    Setup the following files:
    
    1. The MVC Framework from the Exam01 Assignment.
    2. Changed the public htaccess file to reflect the URLROOT of the app.
    3. Added a access modifier check when trying to set the controller method so we can create private methods without introducing bugs.
    4. Setup composer for use in installing php libraries.
    5. Setup the codeception test runner for the feature acceptance tests

---
+++ PokemonMarketplace/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+// App Root
+define('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define('URLROOT', 'localhost/eCommerce22/PokemonMarketplace');
+// Site Name
+define('SITENAME', 'Pokemon Marketplace');
+
+// Database Params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'YOUR_DB_NAME');
