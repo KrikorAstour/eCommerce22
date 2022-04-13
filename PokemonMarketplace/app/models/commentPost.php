521a7a48bbf30bbf777091ddc0315ba24d91d443
Benny <82058278+liu-benny@users.noreply.github.com>
2022-04-13 14:44:26 -0400

    Created commentModel
    - getPostComments()
    - createComment()
    - editComment()
    - deleteComment()

---
+++ PokemonMarketplace/app/models/commentPost.php
@@ -0,0 +1,70 @@
+<?php
+    class commentModel extends Model{
+
+        public function __construct(){
+            parent::__construct();
+        }
+
+        public function getPostComments($post_id){
+            $this->query("SELECT c.comment, c.updated_at,
+                                 u.username 
+                          FROM comments AS c
+                          JOIN users AS u ON c.user_id = u.user_id 
+                          WHERE c.post_id = :post_id");
+
+            $this->bind(":post_id",$post_id);
+
+            return $this->getResultSet();
+        }
+
+        public function createComment($post_id,$user_id,$comment){
+            $this->query("INSERT INTO comments (post_id,user_id,comment) 
+                          VALUES (:post_id,:user_id,:comment");
+
+            $this->bind(":post_id",$post_id);
+            $this->bind(":user_id",$user_id);
+            $this->bind(":comment",$comment);
+
+            if($this->execute()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public function editComment($post_id,$user_id,$comment){
+            $this->query("UPDATE comments 
+                          SET comment = :comment
+                          WHERE post_id = :post_id AND user_id = :user_id");
+
+            $this->bind(":post_id",$post_id);
+            $this->bind(":user_id",$user_id);
+            $this->bind(":comment",$comment);
+
+            if($this->execute()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+
+        public function deleteComment($post_id,$user_id){
+            $this->query("DELETE FROM comments 
+                          WHERE post_id = :post_id AND user_id = :user_id");
+
+            $this->bind(":post_id",$post_id);
+            $this->bind(":user_id",$user_id);
+            
+            if($this->execute()){
+                return true;
+            }
+            else{
+                return false;
+            }
+
+
+        }
+    }
+?>
