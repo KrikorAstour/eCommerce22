684659bb0a4536af380ef972aac18382f6eeeb96
Astour Krikor <72988512+KrikorAstour@users.noreply.github.com>
2022-04-25 15:16:51 -0700

    Update Posts.php

--- PokemonMarketplace/app/controllers/Posts.php
+++ PokemonMarketplace/app/controllers/Posts.php
@@ -17,6 +17,79 @@ class Posts extends Controller
             // to be done
         }
     }
+    
+    public function createCard(){
+            if(!isset($_POST['next'])){
+               $this->view('Post/createCard');
+            }
+            else{
+                $data = [
+                    'card_name' => $_POST['card_name'],
+                    'card_rarity' => $_POST['card_rarity'],
+                    'card_image' => $_POST['card_image'],
+                    'post_title' => $_POST['post_title'],
+                    'post_price' => $_POST['post_price'],
+                    'post_description' => $_POST['post_description']
+                ];
+                
+                $isSucc = $this->PostModel->createCard($data);
+                $card_id = $this->PostModel->getCardId($data['card_image']);
+                
+                $isSucc2 = $this->PostModel->createPost($data, $_SESSION['user_id'], $card_id->card_id);
+                if($isSucc2){
+                    echo 'Uploading the Post!';
+                   echo '<meta http-equiv="Refresh" content="2; url=/eCommerce22/PokemonMarketplace/Profile/my_profile">';
+                }
+        }
+    }
+    
+    public function deletePost($postId)
+    {
+        $userId = $_SESSION['user_id'];
+        $isSucc = $this->PostModel->deletePost($postId);
+        
+        if ($isSucc) {
+            echo '<h5 class="text-danger">Deleting The Post!</h5>';
+            echo '<meta http-equiv="Refresh" content="2; url=/eCommerce22/PokemonMarketplace/Profile/my_profile">';
+                } else {
+            echo '<h1 class="text-danger">Internal Server Error: Something Broke!</h1>';
+            echo '<meta http-equiv="Refresh" content="2; url=/eCommerce22/PokemonMarketplace/Profile/my_profile">';
+                }
+        
+    }
+    
+    public function updatePost($post_Id){
+        $this->view('Post/updatePost');
+        $post = $this->PostModel->getPost($post_Id);
+        $data = [
+            "post" => $post
+        ];
+
+        if(!isset($_POST['confirm'])){
+            $this->view('post/updatePost',$data);
+        }
+        else{
+            
+            $data = [
+                    
+                'post_title' => $_POST['post_title'],
+                'post_text' => $_POST['post_text'],
+                'post_price' => $_POST['post_price'],
+                
+            ];
+
+            if(isset($_POST['is_offered'])){
+                $data['is_offered'] = '1';
+            }
+            else{
+                $data['is_offered'] = '0';
+            }
+            if($this->PostModel->updatePost($data, $post_Id)){
+                    echo 'Updating your post!';
+                    echo '<meta http-equiv="refresh" content="0;url=' . URLROOT . '/profile/my-proffile' . $userId . '" />';
+                }
+            }
+        }
 
     public function save_from_profile($post_id, $profile_id)
     {
