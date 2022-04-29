79cff51186b155b44a63fec0d365dd88b157d5b7
Benny <82058278+benny-itakei@users.noreply.github.com>
2022-04-29 10:18:21 -0400

    Update savedPostModel.php

--- PokemonMarketplace/app/models/savedPostModel.php
+++ PokemonMarketplace/app/models/savedPostModel.php
@@ -5,8 +5,24 @@
             $this->db = new Model;
         }
 
+        public function getUserSavedPosts($user_id){
+            $this->db->query(
+                'SELECT p.post_id, post_title, post_description, price, p.created_at as post_creation, '
+                    . 'card_name, card_rarity, card_image, p.user_id, u.username '
+                    . 'FROM posts as p '
+                    . 'JOIN cards as c ON p.card_id = c.card_id '
+                    . 'JOIN users as u ON p.user_id = u.user_id '
+                    . 'JOIN saves as s ON p.user_id = s.user_id '
+                    . 'WHERE s.user_id = :user_id '
+            );
+            $this->db->bind(":user_id",$user_id);
+            
+
+            return $this->db->getResultSet();
+        }
+
         public function getSavedPost($user_id,$post_id){
-            $this->db->query("SELECT * FROM Saves WHERE user_id = :user_id AND post_id = :post_id");
+            $this->db->query("SELECT * FROM saves WHERE user_id = :user_id AND post_id = :post_id");
             $this->db->bind(":user_id",$user_id);
             $this->db->bind(":post_id",$post_id);
 
