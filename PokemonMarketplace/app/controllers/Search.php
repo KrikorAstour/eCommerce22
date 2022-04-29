7e5fcfcc016004c102f9529d47e5953518d60c02
Krikor Astour <ararad.seller@gmail.com>
2022-04-29 16:12:17 -0700

    updated searchModel and controller

--- PokemonMarketplace/app/controllers/Search.php
+++ PokemonMarketplace/app/controllers/Search.php
@@ -13,21 +13,25 @@
 
             if(isset($_POST['search']) && $_POST['search_type'] == "newFirst"){
                 $data = $_POST['search_text'];
+                $rarity = $_POST['rarity'];
                 $isSucc = $this->searchModel->getNewest($data);
                 $this->view('Search/search', $isSucc);
             }
             else if(isset($_POST['search']) && $_POST['search_type'] == "oldFirst"){
                 $data = $_POST['search_text'];
+                $rarity = $_POST['rarity'];
                 $isSucc = $this->searchModel->getOldest($data);
                 $this->view('Search/search', $isSucc);
             }
             else if(isset($_POST['search']) && $_POST['search_type'] == "highFirst"){
                 $data = $_POST['search_text'];
+                $rarity = $_POST['rarity'];
                 $isSucc = $this->searchModel->getHigherPrice($data);
                 $this->view('Search/search', $isSucc);
             }
             else if(isset($_POST['search']) && $_POST['search_type'] == "lowFirst"){
                 $data = $_POST['search_text'];
+                $rarity = $_POST['rarity'];
                 $isSucc = $this->searchModel->getlowerPrice($data);
                 $this->view('Search/search', $isSucc);
             }
--- PokemonMarketplace/app/models/searchModel.php
+++ PokemonMarketplace/app/models/searchModel.php
@@ -4,36 +4,85 @@
             $this->db = new Model;
         }
 
-       public function getHigherPrice($title){
-           $this->query("SELECT * FROM posts
-                        WHERE  ' ' + post_title + ' ' LIKE '% :title %'
-                        ORDER BY price DESC
-                        ");
-            $this->db->bind(':title', $title);            
-       }
+       public function getHigherPrice($title, $rarity){
+           if($rarity == "all"){
+                $this->query("SELECT * FROM posts
+                WHERE  ' ' + post_title + ' ' LIKE '% :title %'
+                ORDER BY price DESC
+                ");
+                $this->db->bind(':title', $title);
+           }
+           else{
+            $this->query("SELECT * FROM posts
+                            JOIN cards
+                            on posts.card_id = cards.crad_id
+                            WHERE  ' ' + posts.post_title + ' ' LIKE '% :title %'
+                            AND cards.rarity = :rarity
+                            ORDER BY price DESC
+                            ");
+                $this->db->bind(':title', $title);  
+                $this->db->bind(':rarity', $rarity);          
+           } 
+        }
        
-       public function getLowerPrice($title){
-        $this->query("SELECT * FROM posts
-                     WHERE  ' ' + post_title + ' ' LIKE '% :title %'
-                     ORDER BY post_price ASC");
-        $this->db->bind(':title', $title);
-        return $this->db->getResultSet();             
+       public function getLowerPrice($title, $rarity){
+           if($rarity == "all"){
+                $this->query("SELECT * FROM posts
+                WHERE  ' ' + post_title + ' ' LIKE '% :title %'
+                ORDER BY post_price ASC");
+                $this->db->bind(':title', $title);
+                return $this->db->getResultSet();
+           }else{
+                $this->query("SELECT * FROM posts
+                            JOIN cards
+                            on posts.card_id = cards.crad_id
+                            WHERE  ' ' + posts.post_title + ' ' LIKE '% :title %'
+                            AND cards.rarity = :rarity
+                            ORDER BY post_price ASC");
+                $this->db->bind(':title', $title);
+                $this->db->bind(':rarity', $rarity);
+                return $this->db->getResultSet();             
+            }
         }
 
-        public function getNewest($title){
-            $this->query("SELECT * FROM posts
+        public function getNewest($title, $rarity){
+            if($raity == "all"){
+                $this->query("SELECT * FROM posts
                             WHERE  ' ' + post_title + ' ' LIKE '% :title %'
                         ORDER BY updated_at DESC");
-            $this->db->bind(':title', $title);
-            return $this->db->getResultSet();             
+                $this->db->bind(':title', $title);
+                return $this->db->getResultSet(); 
+            }else{
+                $this->query("SELECT * FROM posts
+                            JOIN cards
+                            on posts.card_id = cards.crad_id
+                            WHERE  ' ' + posts.post_title + ' ' LIKE '% :title %'
+                            AND cards.rarity = :rarity
+                            ORDER BY updated_at DESC");
+                $this->db->bind(':title', $title);
+                $this->db->bind(':rarity', $rarity);
+                return $this->db->getResultSet();
+            }             
         }
 
-        public function getOldest($title){
-            $this->query("SELECT * FROM posts
+        public function getOldest($title, $rarity){
+            if($rarity == "all"){
+                $this->query("SELECT * FROM posts
                             WHERE  ' ' + post_title + ' ' LIKE '% :title %'
                         ORDER BY updated_at ASC");
-            $this->db->bind(':title', $title);
-            return $this->db->getResultSet();             
+                $this->db->bind(':title', $title);
+                return $this->db->getResultSet();  
+            }else{
+                $this->query("SELECT * FROM posts
+                                JOIN cards
+                                on posts.card_id = cards.crad_id
+                            WHERE  ' ' + posts.post_title + ' ' LIKE '% :title %'
+                            AND cards.rarity = :rarity
+                            ORDER BY updated_at ASC");
+                $this->db->bind(':title', $title);
+                $this->db->bind(':rarity', $rarity);
+                return $this->db->getResultSet();
+            }             
         }
 
     }
